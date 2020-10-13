961c49a279071dd6017e0258e03ff52d6bb1a218
AlexGr33n <greentr33@gmail.com>
2020-10-13 15:26:30 +0300

    first commit

--- decort_b2b/decort_b2b/urls.py
+++ decort_b2b/decort_b2b/urls.py
@@ -1,7 +1,7 @@
 """decort_b2b URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -13,13 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
-from django.apps import apps
-from django.urls import include, path
 from django.contrib import admin
+from django.urls import path
 
 urlpatterns = [
-    path('i18n/', include('django.conf.urls.i18n')),
     path('admin/', admin.site.urls),
-    path('', include(apps.get_app_config('oscar').urls[0])),
 ]
