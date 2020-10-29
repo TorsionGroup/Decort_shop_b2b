45cd4f424df1e5eb560f746ff340c1dac998e40b
AlexGr33n <greentr33@gmail.com>
2020-10-29 14:05:22 +0200

    fix gunicorn

--- config/decort_b2b.conf.py
+++ config/decort_b2b.conf.py
@@ -1,5 +1,5 @@
 [program:decort_shop_b2b]
-command=/home/prog/www/decort_shop_b2b/oscar/bin/gunicorn decort_shop_b2b.wsgi:application -c /home/prog/www/decort_shop_b2b/config/gunicorn.conf.py
+command=/home/prog/www/decort_shop_b2b/oscar/bin/gunicorn decort_b2b.wsgi:application -c /home/prog/www/decort_shop_b2b/config/gunicorn.conf.py
 directory=/home/prog/www/decort_shop_b2b
 user=prog
 autorestart=true
